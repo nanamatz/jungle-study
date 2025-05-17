1dbe9b1260a64faedf7babc863d221442a8c4ada
DongGyu Kim <shfkemll@gmail.com>
2025-05-17 14:48:04 +0900

    0517

--- week1/달팽이.py
+++ week1/달팽이.py
@@ -1,9 +1,8 @@
 import math
 a,b,v = list(map(int,input().split()))
 
-distance = a-b
-days = math.ceil(v / distance)
-print(days)
-days -= b
-
+k = v - a #도착점 - 순전진 거리 => 도착 직전 지점
+distance = a-b # 일일 전진 거리
+days = math.ceil(k/distance) + 1 #도착 직전 지점까지 걸리는 일 수를 구한 뒤 한 번 더 이동을 한다.
+                                #(도착 직전 지점 상태일 때 기준 밤이므로 이동을 하는 낮으로 바뀌면서 하루가 추가된다.)
 print(days)
---
+++ week1/소수 찾기.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = list(map(int,input().split()))
+count = 0
+for i in range(len(arr)):
+    if arr[i] == 1:
+        continue
+    flag = True
+
+    for j in range(2,arr[i]): # 2~ 자기 자신-1 로 나누어 떨어지면 소수가 아님. 
+        if arr[i] % j == 0:
+            flag = False
+            continue
+    if flag: #한 번도 나누어 떨어지지 않았으면 소수이므로 +1
+        count += 1
+    
+
+print(count)
